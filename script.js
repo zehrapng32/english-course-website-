fea12ebf4692947920acec23df5da91240ae8f8e
ZEHRA NUR <zehraaydogdu494@gmail.com>
2026-05-17 01:49:51 +0300

    updated language switch and website sections

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function closeImage() {
   document.getElementById("imageOverlay").style.display = "none";
 }
 
-
+/*
 const contactForm = document.getElementById("contactForm");
 const successMessage = document.getElementById("successMessage");
 
@@ -23,3 +23,40 @@ contactForm.addEventListener("submit", function(e) {
 
     contactForm.reset();  // bu sayfada form varsa çalıştır yoksa geç için yazıldı 
 });
+
+
+*/
+const languageSwitch = document.getElementById("languageSwitch");
+
+languageSwitch.addEventListener("change", function () {
+
+  if (languageSwitch.checked) {
+        document.getElementById("programText").textContent = "Courses";
+        document.getElementById("teacherText").textContent = "Instructors";
+        document.getElementById("sssText").textContent = "FAQ";
+        document.getElementById("signup").textContent = "Sign Up";
+        document.getElementById("BaslikProgramlar").textContent = " Our Courses";
+        document.getElementById("eğitmenlerBaslik").textContent = "Instructors ";
+        document.getElementById("FAQ").textContent = "Answers to Your Questions ";
+        document.getElementById("contact").textContent = "Contact Us ";
+        document.getElementById("şubeler").textContent = "Our Branches";
+
+
+    } else {
+        document.getElementById("programText").textContent = "Kurslar";
+        document.getElementById("teacherText").textContent = "Eğitmenler";
+        document.getElementById("sssText").textContent = "SSS";
+        document.getElementById("signup").textContent = "Kayıt Ol";
+        document.getElementById("BaslikProgramlar").textContent = "Kurslarımız";
+        document.getElementById("BaslikProgramlar").textContent = " Eğitmenler";
+        document.getElementById("FAQ").textContent = "Aklınızdaki Sorular ";
+        document.getElementById("contact").textContent = "İletişim ";
+        document.getElementById("şubeler").textContent = "Şubelerimiz ";
+
+
+    }
+
+});
+
+
+
